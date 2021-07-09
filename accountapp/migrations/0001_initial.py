04a65bb6fca3865594ec8ceb1b0a32a8f39ede75
jinwonn <nngg375@naver.com>
2021-07-09 13:04:40 +0900

    7/9 4ban class commit4

--- accountapp/migrations/0001_initial.py
+++ accountapp/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='NewMoedel',
+            name='NewModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.CharField(max_length=255)),
