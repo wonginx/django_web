cbf59419762bb63a2cca081b955c8921ae29349e
jinwonn <nngg375@naver.com>
2021-07-09 13:03:29 +0900

    7/9 4ban class commit3

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class NewMoedel(models.Model):
+class NewModel(models.Model):
     text = models.CharField(max_length=255, null=False)
     # CharField 문자열로 받을것  / 255개의 길이 / null false = 꼭 값이 input 되야한다
+    #
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import NewModel
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST" :
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+        
+        return render(request, 'accountapp/hello_world.html',
+                      context = {'new_model': new_model})
+    else:
+        return render(request, 'accountapp/hello_world.html' ,
+                      context = {'text': 'GET METHOD!'})
