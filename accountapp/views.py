a8edc1251927b0c23a2ece041b819cf7a3b71d0b
jinwonn <nngg375@naver.com>
2021-07-13 11:49:05 +0900

    7/13 4ban class commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,24 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+# Create your views here.
+from django.urls import reverse
 from accountapp.models import NewModel
 
 
-# Create your views here.
-
 def hello_world(request):
-    if request.method == "POST":
-
+    if request.method == 'POST':
         temp = request.POST.get('input_text')
-
         new_model = NewModel()
         new_model.text = temp
         new_model.save()
-
-        data_list = NewModel.objects.all()
-
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
