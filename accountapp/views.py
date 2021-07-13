66e89d90c40cf96927e173db01b12f42aa5cbf46
jinwonn <nngg375@naver.com>
2021-07-13 12:39:38 +0900

    7/13 4ban class commit3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 from accountapp.models import NewModel
-
 
 def hello_world(request):
     if request.method == 'POST':
+
         temp = request.POST.get('input_text')
+
         new_model = NewModel()
         new_model.text = temp
         new_model.save()
@@ -16,3 +20,10 @@ def hello_world(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
