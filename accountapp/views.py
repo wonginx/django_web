de6130d50e3557c7d376730ed22f14809e8aed07
jinwonn <nngg375@naver.com>
2021-07-20 11:30:32 +0900

    7/20 UpdateView + 오류수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 from accountapp.models import NewModel
 
 def hello_world(request):
@@ -29,6 +29,13 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView):
-    moedel = User
+    model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = 'accountapp/update.html'
