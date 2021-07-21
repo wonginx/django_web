68669841c055c8b66705a0de1934be8d33e5adb4
jinwonn <nngg375@naver.com>
2021-07-21 16:52:10 +0900

    7/21 hello, Create, Delete 본인인증시스템

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,25 +2,30 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-# Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
+
 def hello_world(request):
-    if request.method == 'POST':
+    # 로그인 여부 확인
+    if request.user.is_authenticated:
 
-        temp = request.POST.get('input_text')
+        if request.method == 'POST':
+            temp = request.POST.get('input_text')
 
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            new_model = NewModel()
+            new_model.text = temp
+            new_model.save()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            data_list = NewModel.objects.all()
+            return render(request, 'accountapp/hello_world.html',
+                          context={'data_list': data_list})
     else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:login:'))
+    # 로그인이 안되어 있다면 로그인페이지로 이동
 
 
 class AccountCreateView(CreateView):
@@ -29,10 +34,12 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
 
 class AccountUpdateView(UpdateView):
     model = User
@@ -40,6 +47,20 @@ class AccountUpdateView(UpdateView):
     context_object_name = 'target_user'
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = 'accountapp/update.html'
+    # def get 회원본인만 업데이트, 딜리트 뷰 수정가능코드
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
 
 class AccountDeleteView(DeleteView):
     model = User
@@ -47,3 +68,15 @@ class AccountDeleteView(DeleteView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
 
+    # def get 회원본인만 업데이트, 딜리트 뷰 수정가능코드
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
