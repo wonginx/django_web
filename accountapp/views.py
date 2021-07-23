842ce729fb9431d6a4112a76010706b4d48aaa18
jinwonn <nngg375@naver.com>
2021-07-23 11:37:32 +0900

    7/23 decorators 생성 및 적용

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 from django.utils.decorators import method_decorator
@@ -42,20 +44,22 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+has_ownership = [login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = 'accountapp/update.html'
-
-
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
-class AccountDeleteView(DeleteView):
-    model = User
-    context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/delete.html'
